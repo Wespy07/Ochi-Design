acaa3e16cff144d9a8906acd12bf9329816cbe18
wespy <wespy07@gmail.com>
2024-11-18 11:58:05 +0530

    initial commit and boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-full flex justify-center items-center bg-gray-900 text-white'>
+      <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
