4f73b8cbe8692c0fc37b5d6fc66dd0e2b47cdd5d
wespy <wespy07@gmail.com>
2024-12-10 14:36:06 +0530

    fix margins

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,8 +7,8 @@ function AboutUs() {
   return (
     <>
         < Navbar />
-        <div className='h-screen w-full flex flex-col justify-center items-center about-us'>
-            <h1>wespy07</h1>
+        <div className='h-screen w-full flex flex-col items-center about-us'>
+            <h1 className='mt-40'>wespy07</h1>
             <p>The closer you look, the lesser you see.</p>
         </div>
         < Footer />
